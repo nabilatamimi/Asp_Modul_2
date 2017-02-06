4489b1c1cbdd0412904a46e595757563843d3c4c
tomama <nabilatamimi09@gmail.com>
2017-02-06 10:45:06 +0700

    Modul2

---
+++ asp_mvc_2/Controllers/AuthorizeRolesAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace asp_mvc_2.Controllers
+{
+    internal class AuthorizeRolesAttribute : Attribute
+    {
+        private string v;
+
+        public AuthorizeRolesAttribute(string v)
+        {
+            this.v = v;
+        }
+    }
+}
